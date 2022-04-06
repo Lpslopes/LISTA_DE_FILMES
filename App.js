48fd4bd603a9be8e42119f50e6d2d19751a36615
Lpslopes <lamartinelps@gmail.com>
2022-04-05 22:59:52 -0300

    Lista_de_Filmes_React_Native

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
+import Filmes from './src/Filmes';
+import api from './src/services/api';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default function App(){
+
+  const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    async function loadFilmes(){
+      const response = await api.get('https://sujeitoprogramador.com/r-api/?api=filmes');
+      setFilmes(response.data);
+      setLoading(false);
+    }
+
+    loadFilmes();
+
+  }, []);
+
+  if(loading){
+    return(
+      <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+        <ActivityIndicator color='#121212' size={45}/>
+      </View>
+    )
+  }else{
+    return(
+      <View style={styles.container}>
+        
+        <FlatList
+        data={filmes}
+        keyExtractor={ item => String(item.id)}
+        renderItem={ ({ item }) => <Filmes data={item}/> }
+        
+        />
+  
+      </View>
+    );
+
+
+  }
+
+ 
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    marginTop:30,
+  }
+})
